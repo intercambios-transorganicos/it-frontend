2019bcaab3cc7652b395772b4184e67b9f49adfa
milasantacruz <milalovechaton@gmail.com>
2021-09-23 00:51:38 -0300

    settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,12 +9,13 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: process.env.DEPLOY_URL ? "https://intercambios-transorganicos.herokuapp.com/" : "http://localhost:1337`",
+        apiURL: process.env.DEPLOY_URL ? "https://intercambios-transorganicos.herokuapp.com/" : "http://localhost:1337",
         queryLimit: 1000, // Defaults to 100
         collectionTypes: [`articulos`],
         singleTypes: [`homepage`],
       },
     },
+    `gatsby-plugin-netlify`,
     `gatsby-plugin-fontawesome-css`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,

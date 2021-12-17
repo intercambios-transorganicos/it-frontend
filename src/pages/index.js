874ccad358e6b8e796f03842da7bbf6c2363b919
milasantacruz <milalovechaton@gmail.com>
2021-12-17 14:26:23 -0300

    cambios en index por pruebas

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import Homepage from "../components/homepage"
-import About1 from "../components/about1"
-import About2 from "../components/about2"
-import About3 from "../components/about3"
-import Loader from "../components/itLoader"
+// import Homepage from "../components/homepage"
+// import About1 from "../components/about1"
+// import About2 from "../components/about2"
+// import About3 from "../components/about3"
+// import Loader from "../components/itLoader"
 import 'bootstrap/dist/css/bootstrap.min.css'
 import "./index.scss"
 

2e171d4d3947bce12c32535ae0dc822cd627d059
milasantacruz <milalovechaton@gmail.com>
2022-05-08 15:31:34 -0300

    cambios en about 1

--- src/components/presentacion.js
+++ src/components/presentacion.js
@@ -12,27 +12,6 @@ const Presentacion = ({titulo, desc}) => {
             <h1>{titulo}</h1>
             <div className="separador"></div>
             <p>{desc}</p>
-          <div className="presButtons">
-          <Link to="/nosotres" >
-           <div className="links_press">
-                 <div className="icon-centered">
-                <FontAwesomeIcon 
-                  size="1x" 
-                  icon={faExternalLinkAlt} />
-                 </div>
-                <h2 className="" >Conoce a nuestro equipo</h2>
-            </div></Link>
-            <Link to="/proyectos" >
-            <div className="links_press">
-                 <div className="icon-centered">
-                <FontAwesomeIcon 
-                  size="1x" 
-                  icon={faExternalLinkAlt} />
-                 </div>
-                <h2 className="" >Proyectos e investigacion</h2>
-            </div>
-            </Link>
-          </div>
         </div>
         </div>
     );

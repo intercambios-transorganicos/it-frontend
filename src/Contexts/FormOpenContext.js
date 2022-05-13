82743e0a5130b868c1de513478f70129f9ffb177
Paloma <paloma@Air-de-Paloma.fibertel.com.ar>
2022-05-13 19:01:54 -0300

    Arreglos en la seccion Aventura (mailForm)

---
+++ src/Contexts/FormOpenContext.js
@@ -0,0 +1,18 @@
+import React, { createContext, useContext, useState } from "react";
+
+const FormOpenContext = createContext();
+
+export const FormOpenContextUse = () => {
+    return useContext(FormOpenContext)
+}
+
+export const FormOpenContextProvider = ({ children }) => {
+
+    const [formOpen, setFormOpen] = useState(false);
+
+    return(
+        <FormOpenContext.Provider value={{ formOpen, setFormOpen }}>
+            {children}
+        </FormOpenContext.Provider>
+    );
+}

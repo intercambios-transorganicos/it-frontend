3edeb4bd78342cad74dbece9f68d8095c6d883b7
Paloma <paloma@Air-de-Paloma.fibertel.com.ar>
2022-05-26 15:23:45 -0300

    Agregue el link al c3d en la secAventura y agregue un titulo al formulario de contacto

--- src/components/MailForm.js
+++ src/components/MailForm.js
@@ -26,13 +26,16 @@ const MailForm = () => {
 
     return (
         <form className='mailForm col-12 row p-0 justify-content-center'>
+
+            <h3 className='tituloForm mt-4 mb-2'>¿Querés decirnos algo?</h3>
+
             <label className='col-9 lab mt-5 mb-1 p-0'>Tu nombre:</label>
             <input className='col-9 inp' onChange={e => setNombre(e.target.value)} type="text"></input>
 
             <label className='col-9 lab mt-5 mb-1 p-0'>Tu email:</label>
             <input className='col-9 inp' onChange={e => setEmail(e.target.value)} type="email"></input>
 
-            <label className='col-9 lab mt-5 mb-1 p-0'>Mensaje:</label>
+            <label className='col-9 lab mt-5 mb-1 p-0'>Tu mensaje:</label>
             <textarea className="col-9 inp inpMSJ" onChange={e => setMsj(e.target.value)}></textarea>
 
             <div className='col-9 my-4 p-0 d-flex justify-content-end'>

a645f14a74adafc8bc30eb0667c9d99f42819409
milasantacruz <milalovechaton@gmail.com>
2022-07-07 23:07:02 -0300

    cambios Nosotres**

--- src/pages/nosotres.js
+++ src/pages/nosotres.js
@@ -1,13 +1,44 @@
 import React from 'react';
-import TeamCarousel from "../components/teamCarousel"
+//import TeamCarousel from "../components/teamCarousel"
 import CirclePath from "../components/circlePath"
 import Layout from "../components/layout"
 import "./nosotres.scss"
-import loadable from "@loadable/component"
+import {graphql} from "gatsby"
+import {GatsbyImage, getImage} from "gatsby-plugin-image"
+//import loadable from "@loadable/component"
 //const MyLoadable2 = loadable(() => import("../components/teamCarousel"),{ssr:true});
 
 
-const Publicaciones = () => {
+const Publicaciones = ({data:{allStrapiMembers:{edges:members}}}) => {
+
+
+  var directora = [];
+  var alumnx = [];
+  var exalumnxs = [];
+  var colaboradorx = [];
+
+  members.map((e,i)=>{
+    var current = e.node;
+    switch(current.rol){
+      case "directora":
+      directora.push(current)
+      break
+      case "alumnx":
+        alumnx.push(current);
+      break
+      case "exalumnxs":
+        exalumnxs.push(current);
+      break
+      case "colaboradorx":
+        colaboradorx.push(current)
+      break
+      default:
+        console.log("error");
+
+    }
+
+  })
+    
     return (
         <Layout>
             <div className="teamText" >
@@ -16,7 +47,107 @@ const Publicaciones = () => {
                
             </div>
             <div className="nosotres">
-              <TeamCarousel/>
+               <div className="direccionWrapper " >
+                <div className="tituloDireccion" >
+                  <h1  className="title">Directora:</h1>
+                </div>
+                {
+                  directora.map((e,i) =>{
+                    
+                    var img = getImage(e.foto[0].localFile)
+                    return(
+                      <div key={i} className="columns memberWrapper" >
+                        <div className="column memberImageContainer" >
+                            <GatsbyImage className="roundPicture" image={img} alt={e.nombre} />
+                        </div>
+                        <div className="column is-four-fifths memberInfoContent" >
+                          <div className="contentTop" >
+                              <h1 className="title">{e.nombre}</h1>
+                              <h1 className="subtitle">{e.titulo}</h1>
+                          </div>
+                          <p className="" >{e.descripcion}</p>
+                        </div>
+                      </div>
+                    )
+                  })
+                }
+               </div>
+
+               <div className="alumnxsWrapper " >
+                <div className="tituloAlumnxs" >
+                  <h1 className="title">Alumnxs De Investigacion</h1>
+                </div>
+                  {
+                    alumnx.map((e,i)=>{
+                      var img = getImage(e.foto[0].localFile)
+                      return(
+                        <div key={i} className="columns memberWrapper" >
+                          <div className="column memberImageContainer" >
+                              <GatsbyImage className="roundPicture" image={img} alt={e.nombre} />
+                          </div>
+                          <div className="column is-four-fifths memberInfoContent" >
+                            <div className="contentTop" >
+                                <h1 className="title">{e.nombre}</h1>
+                                <h1 className="subtitle">{e.titulo}</h1>
+                            </div>
+                            <p className="" >{e.descripcion}</p>
+                          </div>
+                        </div>
+                      )
+                    })
+                  }
+                
+               </div>
+
+               <div className="colaboradorxsWrapper" >
+               <div className="tituloColaboradorxs" >
+                  <h1 className="title">Colaboradorxs</h1>
+                </div>
+                  {
+                    colaboradorx.map((e,i)=>{
+                      var img = getImage(e.foto[0].localFile)
+                      return(
+                        <div key={i} className="columns memberWrapper" >
+                          <div className="column memberImageContainer" >
+                              <GatsbyImage className="roundPicture" image={img} alt={e.nombre} />
+                          </div>
+                          <div className="column is-four-fifths memberInfoContent" >
+                            <div className="contentTop" >
+                                <h1 className="title">{e.nombre}</h1>
+                                <h1 className="subtitle">{e.titulo}</h1>
+                            </div>
+                            <p className="" >{e.descripcion}</p>
+                          </div>
+                        </div>
+                      )
+                    })
+                  }
+               </div>
+
+               <div className="exalumnxsWrapper" >
+               <div className="tituloExalumnxs" >
+                  <h1 className="title">Exalumnxs</h1>
+                </div>
+                  {
+                    exalumnxs.map((e,i)=>{
+                      var img = getImage(e.foto[0].localFile)
+                      return(
+                        <div key={i} className="columns memberWrapper" >
+                          <div className="column memberImageContainer" >
+                              <GatsbyImage className="roundPicture" image={img} alt={e.nombre} />
+                          </div>
+                          <div className="column is-four-fifths memberInfoContent" >
+                            <div className="contentTop" >
+                                <h1 className="title">{e.nombre}</h1>
+                                <h1 className="subtitle">{e.titulo}</h1>
+                            </div>
+                            <p className="" >{e.descripcion}</p>
+                          </div>
+                        </div>
+                      )
+                    })
+                  }
+               </div>
             </div>
                
         </Layout>
@@ -24,3 +155,26 @@ const Publicaciones = () => {
 }
 
 export default Publicaciones;
+
+export const query = graphql`
+  {
+    allStrapiMembers {
+      edges {
+        node {
+          id
+          nombre
+          titulo
+          rol
+          descripcion
+          foto {
+            localFile {
+              childImageSharp {
+                gatsbyImageData(layout: CONSTRAINED, placeholder: DOMINANT_COLOR, aspectRatio:1)
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`

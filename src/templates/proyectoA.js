ba66da217cc2b277bde81ac676dacfc8db663055
milasantacruz <milalovechaton@gmail.com>
2022-09-14 01:23:33 -0300

    links a documentos en proyectos

--- src/templates/proyectoA.js
+++ src/templates/proyectoA.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {graphql} from "gatsby"
 import MediaProyectos from "../components/mediaProyectos"
+import {Link} from "gatsby"
 //import loadable from "@loadable/component"
 import Layout from "../components/layout"
 import InfoExpanded from "../components/infoExpandedx2"
@@ -10,8 +11,22 @@ import "./proyectoA.scss"
 
 const Proyectoa = ({data}) => {
 
-    const{id, nombre, titulo, subtitulo, contenido, imagenes} = data.strapiProyectos;
-  
+    const{id, nombre, titulo, subtitulo, contenido, imagenes, oembed, documents} = data.strapiProyectos;
+
+    if(documents){
+      console.log(documents[0])
+    }else{
+      console.log("no docx")
+    }
+
+
+    if(oembed){
+      var iFrame = JSON.parse(oembed).rawData.html
+      console.log(iFrame)
+    }else{
+      console.log("no oembed")
+    }
+
     return (
         <Layout>
             <div className="proyectoContainer">
@@ -19,14 +34,52 @@ const Proyectoa = ({data}) => {
                     <MediaProyectos imagenes={imagenes} />
                       <div className="portadaTitulo">
                           <h1>{nombre}</h1>
-                          <h2>{titulo}</h2>
                       </div>
                  </div>
 
-                  <div className="proyectoContent">
-                    <InfoExpanded titulo={subtitulo} desc={contenido} bgColor="#eeeeee" txColor={"#242424"} />
-                 
-                  </div>
+                 <div className="section general">
+
+                    <div className="container">
+                        <div className="section info has-text-white">
+                            <h1 className="title has-text-white" >{titulo}</h1>
+                            <p>{contenido}</p>
+                        </div>
+                        {oembed?
+                        <div className="section video">
+                            <div className="iFrame" dangerouslySetInnerHTML={{ __html: iFrame}} /> 
+                        </div>
+                        :
+                        <div></div>
+                        } 
+                        <div className="section links">
+                          {
+                            documents.map((e,i)=>{
+                              var current = e.documento[0].localFile.publicURL
+                              var currentNombre = e.documento[0].name
+                              console.log(current);
+                              return(
+                                <div className="buttons">
+                                  <button className="button bLinks ">
+                                    <a className="has-text-white" href={current}>{currentNombre}</a>
+                                  </button>
+                                </div>
+                              )
+                            })
+                          }
+                          <div className="buttons">
+                              <button className="button bLinks ">
+                              <Link className="has-text-white" to="/proyectos02" > + Proyectos </Link>
+                              </button>
+                          </div>
+                          {/* <div className="buttons">
+                              <button  className="button bLinks has-text-white ">
+                                Contactanos para adquirir este juego
+                              </button>
+                          </div> */}
+                        </div>
+                    </div>
+                 </div>
+
             </div>
         </Layout>
     );
@@ -40,12 +93,24 @@ query Proyecto($proyectoId: String){
       titulo
       subtitulo
       contenido
+      oembed
+      documents {
+        documento {
+          name
+          localFile {
+            publicURL
+          }
+        }
+      }
       imagenes {
         localFile {
           childImageSharp {
             gatsbyImageData(
               placeholder: DOMINANT_COLOR
               layout: CONSTRAINED
+              transformOptions: {fit: COVER, cropFocus: CENTER}
+              width: 1920
+              height: 1080
               )
           }
         }

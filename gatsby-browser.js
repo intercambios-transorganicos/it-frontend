9f2e5618b9cb7aa9de80ac9ee6cf1c2c8c39c1ec
milasantacruz <milalovechaton@gmail.com>
2022-12-02 15:33:02 -0300

    remove loader

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,22 +1,22 @@
 //import React from "react"
-import "./src/components/itLoader.scss"
-import "./styles.css"
+//import "./src/components/itLoader.scss"
+//import "./styles.css"
 //import { FormOpenContextProvider } from "./src/Contexts/FormOpenContext"
-export const onClientEntry = () => {
-    console.log("from browser-out")
-  }
+// export const onClientEntry = () => {
+//     console.log("from browser-out")
+//   }
 
-  export const onInitialClientRender = () => {
-    console.log("from render-out")
-  }
+//   export const onInitialClientRender = () => {
+//     console.log("from render-out")
+//   }
 
-  export const onPreRouteUpdate = () => {
+  /*export const onPreRouteUpdate = () => {
     document.getElementById("preloader").classList.add('fadeOutDownX');
     setTimeout(() => {
       document.getElementById("preloader").setAttribute('hidden', 'true');
       document.getElementsByTagName("body")[0].classList.remove("preloader_active");
     }, 1000)
-}
+}*/
 
 // export const wrapRootElement = ({ element }) => (
 //     <FormOpenContextProvider>

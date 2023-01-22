4cdf7c1f542519c1d8564108d3081b1f6875883b
milasantacruz <milalovechaton@gmail.com>
2023-01-22 15:47:39 -0300

    add analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,20 @@ module.exports = {
   },
   pathPrefix: "/IT-Gatsby",
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-XXCZJ4HEKL", // Google Analytics / GA
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        },
+      },
+    },
     `gatsby-plugin-netlify`,
      {
        resolve: `gatsby-source-strapi`,

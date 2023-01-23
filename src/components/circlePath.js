c945bcdfc895c2c778a3de05b56c76df8506e834
milasantacruz <milalovechaton@gmail.com>
2023-01-23 13:24:45 -0300

    change animation from smile to css circlePath

--- src/components/circlePath.js
+++ src/components/circlePath.js
@@ -9,7 +9,7 @@ const Circlepath = (props) => {
                <path 
                d="M50,250c0-110.5,89.5-200,200-200s200,89.5,200,200s-89.5,200-200,200S50,360.5,50,250" 
                id="textcircle" >
-                   <animateTransform
+                   {/* <animateTransform
                     attributeName="transform"
                     begin="0s"
                     dur="30s"
@@ -17,7 +17,7 @@ const Circlepath = (props) => {
                     from="0 250 250"
                     to="360 250 250"
                     repeatCount="indefinite" 
-                    />
+                    /> */}
                </path>
            </defs>
            <text dy="70" textLength="1220" fill={props.textColor} fontSize="4em">

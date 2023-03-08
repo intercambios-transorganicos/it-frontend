c118026759809094c9bc7f50388ccd12b82516cf
milasantacruz <milalovechaton@gmail.com>
2023-03-07 21:58:24 -0300

    add EmailJS

--- src/components/MailForm.js
+++ src/components/MailForm.js
@@ -1,50 +1,92 @@
-import React, { useEffect, useState } from 'react';
-
+import React, { useEffect, useState, useRef } from 'react';
+import {useFormik} from "formik"
+import * as Yup from "yup"
 import './MailFormStyle.scss';
+import { send } from 'emailjs-com';
 
 const MailForm = () => {
-
     const [goodForm, setGoodForm] = useState(false);
 
-    const [nombre, setNombre] = useState('');
-    const [email, setEmail] = useState('');
-    const [msj, setMsj] = useState('');
+    var iform = useRef();
 
-    useEffect(() => {
-        
-        if(
-            nombre !== '' &&
-            email !== '' &&
-            msj !== ''
-        ){
-            setGoodForm(true);
-        } else{
-            setGoodForm(false);
+    const schema = Yup.object({
+        from_name: Yup.string().required("Required"),
+        from_email: Yup.string().required("Required").email(),
+        message: Yup.string().required("Required")
+    });
+
+    const{
+        handleChange,
+        handleSubmit,
+        touched,
+        values,
+        errors
+    } = useFormik({
+        initialValues: {
+            to_name:'Intercambios Transorganicos',
+            from_name:'',
+            from_email:'',
+            message:''
+        },
+        validationSchema: schema,
+        onSubmit:(values)=>{
+            send(
+                'service_dgbwyxt',
+                'template_mw3vi4h',
+                values,
+                'wH6E5Tie9g3rDwM-H'
+            ).then((response)=>{
+                console.log(response);
+            }).catch((err)=>{
+                console.log(err)
+            })
         }
-
-    },[nombre, email, msj]);
+    })
 
     return (
-        <form className='mailForm col-12 row p-0 justify-content-center'>
+        <form 
+        ref={elem=>{iform.current = elem}}
+        onSubmit={handleSubmit}
+         className='mailForm col-12 row p-0 justify-content-center'>
 
             <h3 className='tituloForm mt-4 mb-2'>¿Querés decirnos algo?</h3>
 
             <label className='col-12 lab mt-5 mb-1 p-0'>Tu nombre:</label>
-            <input className='col-12 inp' onChange={e => setNombre(e.target.value)} type="text"></input>
+            <input className='col-12 inp' 
+            style={{
+                borderColor:touched&&errors.from_name?"red":"green"
+            }}
+            name="from_name"
+            id="from_name"
+            onChange={handleChange} 
+            value={values.from_name}
+            type="text"></input>
 
             <label className='col-12 lab mt-5 mb-1 p-0'>Tu email:</label>
-            <input className='col-12 inp' onChange={e => setEmail(e.target.value)} type="email"></input>
+            <input className='col-12 inp' 
+            style={{
+                borderColor:touched&&errors.from_email?"red":"green"
+            }}
+            name="from_email"
+            id="from_email"
+            onChange={handleChange} 
+            value={values.from_email}
+            type="email"></input>
 
             <label className='col-12 lab mt-5 mb-1 p-0'>Tu mensaje:</label>
-            <textarea className="col-12 inp inpMSJ" onChange={e => setMsj(e.target.value)}></textarea>
+            <textarea 
+            className="col-12 inp inpMSJ" 
+            style={{
+                borderColor:touched&&errors.message?"red":"green"
+            }}
+            name="message"
+            id="message"
+            onChange={handleChange} 
+            value={values.message}
+            ></textarea>
 
             <div className='col-12 my-4 p-0 d-flex justify-content-end'>
-                {
-                goodForm?
-                    <button type="submit" className="enviar">Enviar</button>
-                :
-                    <button disabled type="submit" className="enviar">Enviar</button>
-                }
+            <button type="submit" className="enviar">Enviar</button>
             </div>
         </form>
     )
